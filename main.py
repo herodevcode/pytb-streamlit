e590d7c0f55ea846a1d9df8f1b477f03cda4026e
herodevcode <134185557+herodevcode@users.noreply.github.com>
2023-10-09 23:48:27 +0000

    fixed error

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 def download_video(url, output_path):
     yt = pytube.YouTube(url)
-    video = yt.streams.first()
+    video = yt.streams.get_highest_resolution()
     output_file = os.path.join(output_path, video.default_filename)
-    video.download(output_path=output_path)
+    video.download(output_path=output_path) 
     return output_file
 
 if __name__ == '__main__':
