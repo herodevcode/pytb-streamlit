b2b3cca2bf4c3a20d1baa138ee8a7df872be0492
herodevcode <134185557+herodevcode@users.noreply.github.com>
2023-11-19 07:57:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ def download_video(url, output_path):
     return output_file
 
 if __name__ == '__main__':
+    st.title("YouTube Video Downloader")
     url = st.text_input('YouTube URL')
+
     if url:
-        output_path = 'videos' 
-        os.makedirs(output_path, exist_ok=True)
-        filepath = download_video(url, output_path)
-        with open(filepath,'rb') as f:
-            st.download_button(
-                label='Download video',
-                data=f,
-                file_name=os.path.basename(filepath)
-            )
+        # Button to start the download
+        if st.button('Download Video'):
+            output_path = 'videos'
+            os.makedirs(output_path, exist_ok=True)
+            filepath = download_video(url, output_path)
+
+            with open(filepath, 'rb') as f:
+                st.download_button(
+                    label='Download video',
+                    data=f,
+                    file_name=os.path.basename(filepath),
+                    mime='video/mp4')
